1bf35223237a0947e236019e05bf63c592fe8f3c
jai-sharma <sendtojsharma@gmail.com>
2021-06-14 20:54:05 +0530

    adding hero section with the search bar

---
+++ components/hero-search.jsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  VStack,
+  HStack,
+  Input,
+  Text,
+  Spacer,
+  Button,
+  IconButton,
+} from '@chakra-ui/react'
+import NextImage from 'next/image'
+import { FiSearch as SearchIcon } from 'react-icons/fi'
+
+export default function HeroSearch() {
+  return (
+    <Box bgGradient="linear(135deg, #FFDD00 0%, #FCCD53 100%);" pt="4">
+      <Flex maxW="container.xl" mx="auto">
+        <VStack justify="center" align="flex-start" spacing="4">
+          <Heading
+            fontSize="3xl"
+            fontWeight="bold"
+            fontFamily="Darker Grotesque"
+          >
+            Search for opportunites here
+          </Heading>
+          <HStack spacing="0">
+            <Input
+              minW="lg"
+              w="100%"
+              size="lg"
+              fontSize="sm"
+              bg="white"
+              _hover={{ bg: 'white' }}
+              _focus={{ bg: 'white' }}
+              variant="filled"
+              borderRightRadius="none"
+              colorScheme="whiteAlpha"
+              placeholder="search for jobs, skills, techstack...."
+            />
+            <IconButton
+              size="lg"
+              colorScheme="blue"
+              borderLeftRadius="none"
+              icon={<SearchIcon />}
+            />
+          </HStack>
+          <HStack>
+            <Text>popular :</Text>
+            <Button size="sm" variant="outline" fontWeight="normal">
+              UI/UX
+            </Button>
+            <Button size="sm" variant="outline" fontWeight="normal">
+              Machine Learning
+            </Button>
+            <Button size="sm" variant="outline" fontWeight="normal">
+              Data Science
+            </Button>
+          </HStack>
+        </VStack>
+        <Spacer />
+        <NextImage src="/static/design-guy.png" width="305" height="339" />
+      </Flex>
+    </Box>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import { Box, Center } from '@chakra-ui/react'
 import Navbar from 'components/navbar'
+import HeroSearch from 'components/hero-search'
 
 export default function Home() {
   return (
     <Box as="main">
       <Navbar />
+      <HeroSearch />
     </Box>
   )
 }
