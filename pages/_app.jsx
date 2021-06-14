00c1bfd3e20db77dbaf447bf57004ec56b9b7928
jai-sharma <sendtojsharma@gmail.com>
2021-06-14 20:53:44 +0530

    adding custom font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import NextHead from 'components/next-head'
 import GlobalCSS from 'styles/globals'
+import "@fontsource/darker-grotesque"
 
 export default function MyApp({ Component, pageProps }) {
   return (
