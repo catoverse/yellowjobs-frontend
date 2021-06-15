d582aca84caa4d217daba9eabddaae96de5ea6b0
jai-sharma <sendtojsharma@gmail.com>
2021-06-15 18:40:26 +0530

    adding alternate bg for tweet

--- components/tweet-list.jsx
+++ components/tweet-list.jsx
@@ -10,43 +10,45 @@ import { Tweet } from 'react-static-tweets'
 
 import ContentEmoji from 'assets/categories/content.svg'
 
-const CategoryBasedTweets = ({ category }) => {
+const CategoryBasedTweets = ({ category, isLightGray }) => {
   return (
-    <Box mt={{ base: 0, md: 24 }} py={10}>
-      <HStack>
-        <Center
-          bgGradient="linear(135deg, #FFDD00 0%, #FCCD53 100%)"
-          w="8"
-          h="8"
-          mt="1"
-          borderRadius="md"
+    <Box mt={{ base: 0, md: 24 }} py={10} bg={isLightGray ? 'gray.50' : ''}>
+      <Box maxW="container.xl" mx="auto" px="4">
+        <HStack>
+          <Center
+            bgGradient="linear(135deg, #FFDD00 0%, #FCCD53 100%)"
+            w="8"
+            h="8"
+            mt="1"
+            borderRadius="md"
+          >
+            <ContentEmoji />
+          </Center>
+          <Heading
+            fontSize={{ base: '28px', md: '40px' }}
+            fontWeight="extrabold"
+            fontFamily="Darker Grotesque"
+          >
+            {category} opportuites
+          </Heading>
+        </HStack>
+        <SimpleGrid
+          columns={{ base: 1, sm: 2, md: 3 }}
+          spacing={5}
+          mt="5"
+          alignItems="center"
         >
-          <ContentEmoji />
-        </Center>
-        <Heading
-          fontSize={{ base: '28px', md: '40px' }}
-          fontWeight="extrabold"
-          fontFamily="Darker Grotesque"
-        >
-          {category} opportuites
-        </Heading>
-      </HStack>
-      <SimpleGrid
-        columns={{ base: 1, sm: 2, md: 3 }}
-        spacing={5}
-        mt="5"
-        alignItems="center"
-      >
-        <Tweet id="1404411232669638660" />
-        <Tweet id="1404411232669638660" />
-        <Tweet id="1404411232669638660" />
-        <Tweet id="1404411232669638660" />
-        <Tweet id="1404411232669638660" />
-        <Tweet id="1404411232669638660" />
-      </SimpleGrid>
-      <Button w="full" mt={10} colorScheme="blue">
-        See all {category.toLowerCase()} opportunities
-      </Button>
+          <Tweet id="1404411232669638660" />
+          <Tweet id="1404411232669638660" />
+          <Tweet id="1404411232669638660" />
+          <Tweet id="1404411232669638660" />
+          <Tweet id="1404411232669638660" />
+          <Tweet id="1404411232669638660" />
+        </SimpleGrid>
+        <Button w="full" mt={10} variant="outline" colorScheme="blue">
+          See all {category.toLowerCase()} opportunities
+        </Button>
+      </Box>
     </Box>
   )
 }
@@ -63,12 +65,10 @@ export default function TweetList() {
     'Other',
   ]
   return (
-    <Box bg="gray.50">
-      <Box maxW="container.xl" mx="auto" px="4">
-        {categories.map((category) => (
-          <CategoryBasedTweets category={category} />
-        ))}
-      </Box>
-    </Box>
+    <>
+      {categories.map((category, idx) => (
+        <CategoryBasedTweets category={category} isLightGray={idx % 2 === 0} />
+      ))}
+    </>
   )
 }
