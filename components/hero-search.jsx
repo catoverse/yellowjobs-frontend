2ca4988c354de082891d49eb89b8e620a27902d1
jai-sharma <sendtojsharma@gmail.com>
2021-06-16 10:42:42 +0530

    adding the shadow outside container

--- components/hero-search.jsx
+++ components/hero-search.jsx
@@ -15,11 +15,12 @@ import SearchBar from './searchbar'
 export default function HeroSearch() {
   return (
     <Box
-      as="main"
-      bgGradient="linear(135deg, #FFDD00 0%, #FCCD53 100%)"
       pt="4"
-      ml={{ base: 0, md: 4 }}
+      as="main"
       borderRadius="sm"
+      overflow="hidden"
+      ml={{ base: 0, md: 4 }}
+      bgGradient="linear(135deg, #FFDD00 0%, #FCCD53 100%)"
     >
       <Flex mx="auto" maxW="1200px" px="4">
         <VStack justify="center" align="flex-start" spacing="4" my={24}>
@@ -56,7 +57,10 @@ export default function HeroSearch() {
           </Flex>
         </VStack>
         <Spacer />
-        <Flex display={{ base: 'none', lg: 'flex' }} filter="drop-shadow(0 0 20px yellow)">
+        <Flex
+          display={{ base: 'none', lg: 'flex' }}
+          filter="drop-shadow(0 0 20px yellow)"
+        >
           <NextImage src="/static/design-guy.png" width="305" height="339" />
         </Flex>
       </Flex>
