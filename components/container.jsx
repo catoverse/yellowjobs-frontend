d4a1f98368ae835d7f87ecb100cb3a879a88e9e9
jai-sharma <sendtojsharma@gmail.com>
2021-06-18 08:37:26 +0530

    adding size prop to the container

--- components/container.jsx
+++ components/container.jsx
@@ -1,8 +1,8 @@
 import { Box } from '@chakra-ui/react'
 
-export default function Container({ children }) {
+export default function Container({ children, size }) {
   return (
-    <Box maxW="container.xl" mx="auto" px="4">
+    <Box maxW={size === 'lg' ? 'container.xl' : '1200px'} mx="auto" px="4">
       {children}
     </Box>
   )
