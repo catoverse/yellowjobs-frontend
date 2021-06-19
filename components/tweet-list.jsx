dc2eedbfa16241f67c05e62eec7f5343ef751c2f
jai-sharma <sendtojsharma@gmail.com>
2021-06-19 11:29:23 +0530

    simplyfing a lot of tweet layout

--- components/tweet-list.jsx
+++ components/tweet-list.jsx
@@ -1,25 +1,38 @@
-import CategoryBasedTweets from 'components/category-based-tweets'
+import styled from '@emotion/styled'
+import Container from './container'
+import { Tweet } from 'react-static-tweets'
+import { Box } from '@chakra-ui/react'
+
+const MasonaryGrid = styled(Box)`
+  display: flex;
+  gap: 16px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-width: 400px;
+  max-height: 1000px;
+
+  @media (max-width: 30rem) {
+    flex-direction: row;
+    gap: 1rem;
+    max-height: none;
+  }
+`
 
 export default function TweetList() {
-  const categories = [
-    'Tech',
-    'Design',
-    'Management',
-    'Marketing',
-    'Sales',
-    'Content',
-    'Support',
-    'Others',
-  ]
   return (
-    <>
-      {categories.map((category, idx) => (
-        <CategoryBasedTweets
-          key={idx}
-          category={category}
-          isLightGray={idx % 2 === 0}
-        />
-      ))}
-    </>
+    <Box as="section" my={5}>
+      <Container>
+        <MasonaryGrid mt="5">
+          <Tweet id="1404411232669638660" />
+          <Tweet id="1403847095602216961" />
+          <Tweet id="1403845000740364291" />
+          <Tweet id="1401811181128458240" />
+          <Tweet id="1400452232135405568" />
+          <Tweet id="1400452232135405568" />
+          <Tweet id="1400452232135405568" />
+          <Tweet id="1400452232135405568" />
+        </MasonaryGrid>
+      </Container>
+    </Box>
   )
 }
