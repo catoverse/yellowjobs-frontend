78097ec66fbedac4b9c2cde64d2e83484745d7c8
anshul_ubuntu <ans29hul@gmail.com>
2021-07-12 14:56:28 +0530

    fix

--- components/verification-button.jsx
+++ components/verification-button.jsx
@@ -6,8 +6,8 @@ import { API_URL } from 'lib/api'
 export default function VerificationButton(props) {
     const [isDone, setIsDone] = useState(false);
     function verifiedTweet(isAccepted,id){
+        
         console.log(id,isAccepted);
-
 
     const requestOptions = {
         method: 'POST',
@@ -15,14 +15,14 @@ export default function VerificationButton(props) {
         body: JSON.stringify({ auth: "xyz", tweetId:id , accepted:isAccepted?"approved":"rejected"})
     };
     fetch(`${API_URL}/api/verify`, requestOptions)
-        .then(response => response.json())
-        .then(data => console.log(data));
+        // .then(response => response.json())
+        // .then(data => console.log(data));
 
         setIsDone(true);
     }
     return(    
             <ButtonGroup variant="outline" spacing="6" isDisabled={isDone}>
-                <Button colorScheme="green" onClick={() =>verifiedTweet(true,props.id)}>Accept</Button>
+                <Button colorScheme="green" onClick={() =>verifiedTweet(true,props.id)}>Approve</Button>
                 <Button colorScheme="red" onClick={() =>verifiedTweet(false,props.id)} >Reject</Button>
             </ButtonGroup>
     )
