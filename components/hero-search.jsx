8ecde34894da2966eb2bb3a5e8112d16c21c29ad
Kushagra Misra <misra.kushagra@yahoo.in>
2021-07-19 21:12:54 +0530

    Remove the image from header + make header smaller

--- components/hero-search.jsx
+++ components/hero-search.jsx
@@ -4,19 +4,17 @@ import Container from './container'
 import LoopHeading from './loop-heading'
 import PopularButton from './popular-button'
 import SearchBar from './search-bar'
-import designGuyImageSrc from 'public/static/design-guy.png'
 
 export default function HeroSearch() {
   return (
     <Box
-      pt="4"
       as="main"
       overflow="hidden"
       bgGradient="linear(135deg, #FFDD00 0%, #FCCD53 100%)"
     >
       <Container>
         <SimpleGrid columns={{ base: 1, md: 2 }}>
-          <VStack justify="center" align="flex-start" spacing="4" my={24}>
+          <VStack justify="center" align="flex-start" spacing="4" my={12}>
             <LoopHeading />
             <SearchBar />
             <Flex gridColumnGap="2" direction={{ base: 'column', md: 'row' }}>
@@ -28,13 +26,6 @@ export default function HeroSearch() {
               </HStack>
             </Flex>
           </VStack>
-          <Flex
-            display={{ base: 'none', lg: 'flex' }}
-            filter="drop-shadow(0 0 20px yellow)"
-            justify="flex-end"
-          >
-            <NextImage alt="Design Guy" src={designGuyImageSrc} />
-          </Flex>
         </SimpleGrid>
       </Container>
     </Box>
