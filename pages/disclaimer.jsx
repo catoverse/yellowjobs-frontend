2450ece2cfa8d7be86a574406e263f736b686c61
anshul_ubuntu <ans29hul@gmail.com>
2021-07-27 12:40:54 +0530

    fix

--- pages/disclaimer.jsx
+++ pages/disclaimer.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import Navbar from 'components/navbar'
-import Footer from 'components/footer'
 import { Box, Text, Heading } from "@chakra-ui/react"
 import path from 'path'
 import fs from 'fs'
@@ -21,7 +20,7 @@ const DisclaimerPage = ({ disclaimer }) => {
           </Text>
         ))}
       </Box>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
