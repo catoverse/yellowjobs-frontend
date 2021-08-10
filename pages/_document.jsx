c5d6ddf0c7ec4f26a5fa543e3599eb8d0613ac03
anshul_ubuntu <ans29hul@gmail.com>
2021-08-10 20:27:29 +0530

    og meta placeholder

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,6 +7,42 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            name="description"
+            content="Curated remote job posts updated every 10 minutes. Across tech, design, sales, content and a lot more!"
+          />
+
+          <meta property="og:url" content="https://www.yellowjobs.org/" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="YellowJobs-Helping you find your dream remote gig #remotejob"
+          />
+          <meta
+            property="og:description"
+            content="Curated remote job posts updated every 10 minutes. Across tech, design, sales, content and a lot more!"
+          />
+          <meta
+            property="og:image"
+            content="https://via.placeholder.com/1200x630.png/09f/fff%20C/O%20https://placeholder.com/"
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="yellowjobs.org" />
+          <meta property="twitter:url" content="https://www.yellowjobs.org/" />
+          <meta
+            name="twitter:title"
+            content="YellowJobs - Helping you find your dream remote gig #remotejob"
+          />
+          <meta
+            name="twitter:description"
+            content="Curated remote job posts updated every 10 minutes. Across tech, design, sales, content and a lot more!"
+          />
+          <meta
+            name="twitter:image"
+            content="https://via.placeholder.com/1200x630.png/09f/fff%20C/O%20https://placeholder.com/"
+          />
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
