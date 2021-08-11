3d0f754ef3a78f8a71edfb3cf8164ec559e754f0
anshul_ubuntu <ans29hul@gmail.com>
2021-08-11 19:38:42 +0530

    preview text

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta
             name="description"
-            content="Curated remote job posts updated every 5 minutes. Across tech, design, sales, content and a lot more!"
+            content="Curated remote job posts updated every minutes. Across tech, design, sales, content and a lot more!"
           />
 
           <meta property="og:url" content="https://www.yellowjobs.org/" />
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="Curated remote job posts updated every 5 minutes. Across tech, design, sales, content and a lot more!"
+            content="Curated remote job posts updated every minutes. Across tech, design, sales, content and a lot more!"
           />
           <meta
             property="og:image"
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
           />
           <meta
             name="twitter:description"
-            content="Curated remote job posts updated every 5 minutes. Across tech, design, sales, content and a lot more!"
+            content="Curated remote job posts updated every minutes. Across tech, design, sales, content and a lot more!"
           />
           <meta
             name="twitter:image"
