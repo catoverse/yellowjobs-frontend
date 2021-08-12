971aa9981dee859a7a6bc6565af115d4acf8d699
Kushagra Misra <misra.kushagra@yahoo.in>
2021-08-12 14:01:36 +0530

    Fix Footer layout for wide screens

--- components/footer.jsx
+++ components/footer.jsx
@@ -18,65 +18,73 @@ import DOLogo from './icons/DO.svg'
 export default function Footer() {
   return (
     <Box>
-      <Container maxW="container.xl" centerContent bg="white">
-        <Flex
-          flexDirection={{ base: 'column', md: 'row' }}
-          py={{ base: '2', md: '4' }}
-          justify="space-between"
-          w="100%"
-        >
+      <Container maxW="container.full" centerContent bg="white">
+        <Container maxW="container.xl">
           <Flex
-            align="center"
             flexDirection={{ base: 'column', md: 'row' }}
-            py="4"
-            px={{ base: '8', md: '20' }}
+            py={{ base: '2', md: '4' }}
+            justify="space-between"
+            w="100%"
           >
-            <NextLink href="/" aria-label="YellowJobs Logo">
-              <Link>
-                <YellowJobsLogo />
-              </Link>
-            </NextLink>
-            <Spacer />
-            <Flex p="4" w="full" justify="space-between">
-              <Box _hover={{ color: 'gray.500' }} mx="5">
-                <NextLink href="/about" aria-label="About Us">
-                  About Us
-                </NextLink>
-              </Box>
+            <Flex
+              align="center"
+              flexDirection={{ base: 'column', md: 'row' }}
+              py="4"
+            >
+              <NextLink href="/" aria-label="YellowJobs Logo">
+                <Link>
+                  <YellowJobsLogo />
+                </Link>
+              </NextLink>
               <Spacer />
-              <Box _hover={{ color: 'gray.500' }} mx="5">
-                <NextLink href="/disclaimer" aria-label="Disclaimer">
-                  Disclaimer
-                </NextLink>
-              </Box>
+              <Flex p="4" w="full" justify="space-between">
+                <Box _hover={{ color: 'gray.500' }} mx="5">
+                  <NextLink href="/about" aria-label="About Us">
+                    About Us
+                  </NextLink>
+                </Box>
+                <Spacer />
+                <Box _hover={{ color: 'gray.500' }} mx="5">
+                  <NextLink href="/disclaimer" aria-label="Disclaimer">
+                    Disclaimer
+                  </NextLink>
+                </Box>
+              </Flex>
+            </Flex>
+            <Divider display={{ base: 'block', md: 'none' }} />
+            <Flex
+              align="center"
+              flexDirection={{ base: 'column', md: 'row' }}
+              py="4"
+            >
+              {/*<Link href="mailto:hello@yellowjobs.org" fontWeight="bold">
+              hello@yellowjobs.org
+            </Link>
+            <Spacer />*/}
+              <Flex w="full" justify="space-between">
+                <Link mx="5" href="https://github.com/catoverse/" isExternal>
+                  <GitHubIcon />
+                </Link>
+                <Spacer />
+                <Link
+                  mx="5"
+                  href="https://twitter.com/yellowjobsorg"
+                  isExternal
+                >
+                  <TwitterIcon />
+                </Link>
+                <Spacer />
+                <Link
+                  mx="5"
+                  href="https://www.linkedin.com/company/navgurukul/"
+                  isExternal
+                >
+                  <LinkedInIcon />
+                </Link>
+              </Flex>
             </Flex>
           </Flex>
-          <Divider display={{ base: 'block', md: 'none' }} />
-          <Flex
-            align="center"
-            flexDirection={{ base: 'column', md: 'row' }}
-            py="4"
-            px={{ base: '8', md: '20' }}
-          >
-            {/*<Link href="mailto:hello@yellowjobs.org" fontWeight="bold">
-            hello@yellowjobs.org
-          </Link>
-          <Spacer />*/}
-            <Flex p="4" w="full" justify="space-between">
-              <Link mx="5" href="https://github.com/catoverse/" isExternal>
-                <GitHubIcon />
-              </Link>
-              <Spacer />
-              <Link mx="5" href="https://twitter.com/yellowjobsorg" isExternal>
-                <TwitterIcon />
-              </Link>
-              <Spacer />
-              <Link mx="5" href="https://www.linkedin.com/company/navgurukul/" isExternal>
-                <LinkedInIcon />
-              </Link>
-            </Flex>
-          </Flex>
-        </Flex>
+        </Container>
       </Container>
       <Container maxW="container.xl" centerContent>
         <Flex
@@ -84,18 +92,14 @@ export default function Footer() {
           align="center"
           w="100%"
         >
-          <Text
-            px={{ base: '8', md: '20' }}
-            py={{ base: '4', md: '6' }}
-            fontSize="xs"
-          >
+          <Text py={{ base: '4', md: '6' }} fontSize="xs">
             Made with ❤ in India️
           </Text>
           <Spacer />
           <Divider display={{ base: 'block', md: 'none' }} />
           <Spacer />
 
-          <Flex align="center" py='2'>
+          <Flex align="center" py="2">
             Powered by
             <Link
               href="https://vercel.com/?utm_source=Yellowjobs&utm_campaign=oss"
