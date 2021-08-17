33d8ab0d1e7521692065e56733f10102b5febee5
anshul_ubuntu <ans29hul@gmail.com>
2021-08-18 00:31:57 +0530

    added chakra-modal for login

--- pages/_offline.jsx
+++ pages/_offline.jsx
@@ -1,7 +1,21 @@
 import * as React from 'react'
-
+import Navbar from 'components/navbar'
+import { Box, Center, Heading } from '@chakra-ui/react'
+import Footer from 'components/footer'
 const OfflinePage = () => {
-  return <>Offline</>
+  return (
+    <>
+      <Navbar />
+      <Box m="1.5rem">
+        <Center>
+          <Heading as="h2" size="2xl" p="1rem">
+            Seems like you are not connented to the internet :({' '}
+          </Heading>
+        </Center>
+      </Box>
+      <Footer />
+    </>
+  )
 }
 
 export default OfflinePage
