38491ff96f9e570a80060a0704f2bada7daa2ddf
anshul_ubuntu <ans29hul@gmail.com>
2021-08-18 11:47:00 +0530

    added https to the env link

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa')
 if (process.env.NEXT_PUBLIC_VERCEL_URL)
-  process.env['NEXTAUTH_URL'] = process.env.NEXT_PUBLIC_VERCEL_URL
-console.log('My URL: ', process.env.NEXTAUTH_URL)
+  process.env['NEXTAUTH_URL'] = 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
+console.log('NEXTAUTH_URL: ', process.env.NEXTAUTH_URL)
 module.exports = withPWA({
   pwa: {
     dest: 'public',
