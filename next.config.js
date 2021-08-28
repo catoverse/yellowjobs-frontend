bdfbb7afb50d8e7f5c8c8b7d41bf95ffb270ae00
anshul_ubuntu <ans29hul@gmail.com>
2021-08-28 23:59:50 +0530

    change env vars in config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+if(process.env.NEXT_PUBLIC_VERCEL_ENV==="production")
+  process.env['NEXTAUTH_URL'] = 'https://yellowjobs.org' 
+else if(process.env.NEXT_PUBLIC_VERCEL_URL) {
   process.env['NEXTAUTH_URL'] = 'http://' + process.env.NEXT_PUBLIC_VERCEL_URL
+  console.log('NEXTAUTH_URL: ', process.env.NEXTAUTH_URL)
 }
-console.log('NEXTAUTH_URL: ', process.env.NEXTAUTH_URL)
 
 const withPWA = require('next-pwa')
 module.exports = withPWA({
